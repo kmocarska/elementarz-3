d90bf47153313e7018b8b41e8aa3171288b38143
Grzegorz Dziemiński <grigi.dz@gmail.com>
2013-12-08 10:57:42 +0100

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -5,18 +5,18 @@ int main() {
   int c=0, i, i2, tabelka[256];
 
 //czyszczenie tablicy
-for (i = 0; i<=255; i++){
+for (i = 0; i<=255; i++) {
   tabelka[i] = 0;
 }
 
 //czytanie
-while ((c=getchar())!=EOF){
+while ((c=getchar())!=EOF) {
   tabelka[c]++;
 }
 
 //liczenie i formatowanie w tabelke
 printf("\nznak\tliczba znakow\n-----------------------\n");
-for (i = 32; i<=255; i++){
+for (i = 32; i<=255; i++) {
   if (tabelka[i] > 0){
     printf("%c\t%d\t", i, tabelka[i]);
     for(i2 = 1; i2 <= tabelka[i]; i2++){
